9a94f91364893071094fa9bbc81c4e8ecc05bc34
Mingcai SHEN <archsh@gmail.com>
2018-03-11 00:31:02 +0800

    Use pg_md5 to hash PCP_PASSWORD.

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+export PCP_PASSWORD_MD5=`pg_md5 ${PCP_PASSWORD}`
+
 /usr/bin/gucci /usr/share/pgpool2/pcp.conf.template > /etc/pgpool2/pcp.conf
 /usr/bin/gucci /usr/share/pgpool2/pgpool.conf.template > /etc/pgpool2/pgpool.conf
 /usr/bin/gucci /usr/share/pgpool2/pool_hba.conf.template > /etc/pgpool2/pool_hba.conf
